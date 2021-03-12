abf90a1036b556d53eb6e125cc16168a8a534831
Ankit Pithalia <apithalia@teksystems.com>
2021-03-12 11:44:03 +0530

    Add project files.

---
+++ CsharpEndProject/Teacher.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CsharpEndProject
+{
+        
+        static class Iteacher
+        {
+            public static void AddTeacher()
+            {
+                string dir = Directory.GetCurrentDirectory();
+                string path = dir + "\\data.txt";
+                if (!File.Exists(path))
+                {
+                    using (StreamWriter sw =  File.CreateText(path))
+                    {
+                        sw.WriteLine("=========Teacher Record===========");
+                    }
+                }
+                    
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        Console.WriteLine("Enter Name Of The Teacher");
+                        sw.Write("Name: " + Console.ReadLine());
+                        Console.WriteLine("Enter Class And Section Of The Teacher");
+                        sw.Write(", ClassAndSection: " + Console.ReadLine());
+                        Console.WriteLine("Enter ID");
+                        sw.Write(", ID: " + Console.ReadLine());
+                        sw.WriteLine("");
+                    }
+                
+                
+            }
+            public static void GetTeacherList()
+            {
+                string dir = Directory.GetCurrentDirectory();
+                string path = dir + "\\data.txt";
+                //Console.WriteLine(path);
+                if (!File.Exists(path))
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine("============Teacher Record===========");
+                    }
+                }
+               
+                    using(StreamReader sr = File.OpenText(path))
+                    {
+                        string s = "";
+                        while((s = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                
+            }
+        public static void UpdateTeacher(string id)
+        {
+            bool flag = false;
+            string dir = Directory.GetCurrentDirectory();
+            string path = dir + "\\data.txt";
+            string[] file = File.ReadAllLines(path);
+            StreamWriter writer = File.CreateText(path);
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("=========Teacher Record===========");
+                }
+            }
+            int count = 0;
+            foreach(string line in file)
+            {
+                //Console.WriteLine(line);
+                if (line.Contains(id))
+                {
+                    flag = true;
+                    file[count] = " ";
+                    Console.WriteLine("Enter Updated Name Of The Teacher");
+                   string nm = "Name: " + Console.ReadLine();
+                    Console.WriteLine("Enter Updated Class And Section Of The Teacher");
+                    string cs = ", ClassAndSection: " + Console.ReadLine();
+
+                    file[count] = nm + cs + ", id: " + id;
+                    //Console.WriteLine(file[count] + "  If");
+                    writer.WriteLine(file[count]);
+                }
+                else
+                {
+                    //Console.WriteLine(file[count] + "  else");
+                    writer.WriteLine(line);
+                    count++;
+                    
+                    //Console.WriteLine("Teacher Id not Found");
+                }
+            }
+            if(!flag)
+            {
+                Console.WriteLine("Id not Found");
+            }
+
+
+            writer.Close();
+        }
+
+    }
+}
---
+++ CsharpEndProject/TeacherRecords.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CsharpEndProject
+{
+    class TeacherRecords
+    {
+        public static void Do()
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Please Select One option from below:");
+                Console.WriteLine("1.Add Data\n2.List Data\n3. Update Data");
+                int option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                {
+                    AddData();
+                }
+                else if(option ==2)
+                {
+
+                    ListData();
+                }
+                else
+                {
+                    UpdateData();
+                }
+                Console.WriteLine("\n\nWant to continue?Want to add or list data?(Type yes/no)");
+                input = Console.ReadLine();
+
+            } while (input.Equals("yes"));
+        }
+        private static void AddData()
+        {
+            string input;
+            do
+            {
+                
+                Console.WriteLine("\n========================== Adding Data of the Teacher record File =========================\n");
+                Iteacher.AddTeacher();
+                
+                
+                Console.WriteLine("\n\nWant to add more data?(Type yes/no)");
+                input = Console.ReadLine();
+
+            } while (input.Equals("yes"));
+        }
+        private static void ListData()
+        {
+            Console.WriteLine("\n========================== List Data of the Teacher record File ==========================\n");
+            Iteacher.GetTeacherList();
+        }
+        private static void UpdateData()
+        {
+            string input;
+            do
+            {
+                string id;
+                Console.WriteLine("\n========================= Updating Data of the Teacher record File ==========================\n");
+                Console.WriteLine("Enter the Id of the teacher: ");
+                id = Console.ReadLine();
+                Iteacher.UpdateTeacher(id);
+                Console.WriteLine("\n\nWant to update more data?(Type yes/no)");
+                input = Console.ReadLine();
+
+            } while (input.Equals("yes"));
+        }
+    }
+}
